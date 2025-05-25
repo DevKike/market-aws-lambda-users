adf7a82e92573ade83c44cb0284881238ef90132
DevKike <alvaronarvaez.dev@gmail.com>
2025-05-24 21:22:58 -0500

    Feat: add global secondary index configuration to DynamoDB table

--- terraform/modules/dynamo-db/main.tf
+++ terraform/modules/dynamo-db/main.tf
@@ -8,4 +8,16 @@ resource "aws_dynamodb_table" "table" {
     type = var.hash_key_type
   }
 
+  attribute {
+    name = var.gsi_key
+    type = var.gsi_key_type
+  }
+
+  global_secondary_index {
+    name            = var.gsi_name
+    hash_key        = var.gsi_key
+    projection_type = var.gsi_projection_type
+    read_capacity   = 5
+    write_capacity  = 5
+  }
 }
